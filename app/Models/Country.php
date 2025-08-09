2e849973c586676b18d8b036bfebc754aa5c0e8d
Wazhma Hakimi <133529903+WazhmaHakimi@users.noreply.github.com>
2025-08-09 19:45:01 +0430

    Filament added along with migrations and models

---
+++ app/Models/Country.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $fillable = [
+        'country_code', 
+        'name'
+    ];
+
+    public function states()
+    {
+        return $this->hasMany(State::class);
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+}
